e41ca2c46b787473526b8fc7728244d6f71db34d
Ikenna Omekam <iomekam@github.com>
2021-02-18 00:41:34 -0500

    add shopify_product

--- app/consumers/cms_product_created_consumer.rb
+++ app/consumers/cms_product_created_consumer.rb
@@ -2,10 +2,16 @@
 
 class CmsProductCreatedConsumer
   include Hutch::Consumer
-  consume "user.vendor.created"
+  consume "cms.product.added"
   queue_name "consumer_ecommerce_orchestrator_cms_product_created"
 
   def process(message)
-
+    CreateShopifyProductJob.perform_later(
+      user_id: message[:product][:owner_id],
+      name: message[:product][:name],
+      price: message[:product][:price],
+      type: message[:product_type],
+      tags: ["cms_id:#{message[:product_id]}"]
+    )
   end
 end
