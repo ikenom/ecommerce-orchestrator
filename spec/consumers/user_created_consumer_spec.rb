6930ec3bdbcc627fc47c09443cfa239c9413fa68
Ikenna Omekam <iomekam@github.com>
2021-02-25 01:42:25 -0500

    lint

--- spec/consumers/user_created_consumer_spec.rb
+++ spec/consumers/user_created_consumer_spec.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 RSpec.describe UserCreatedConsumer do
-  let(:message) {
-   {
-     sender_id: Faker::Alphanumeric.alpha,
-     auth_id: Faker::Alphanumeric.alpha
-   }
-  }
+  let(:message) do
+    {
+      sender_id: Faker::Alphanumeric.alpha,
+      auth_id: Faker::Alphanumeric.alpha
+    }
+  end
 
   subject(:consumer) { described_class.new }
 
@@ -21,8 +21,8 @@ RSpec.describe UserCreatedConsumer do
   it "should enqueue user created job" do
     consumer.process(message)
     expect(UserCreatedJob).to have_been_enqueued.with({
-                                                       user_id: message[:sender_id],
-                                                       auth_id: message[:auth_id]
-                                                     })
+                                                        user_id: message[:sender_id],
+                                                        auth_id: message[:auth_id]
+                                                      })
   end
 end
