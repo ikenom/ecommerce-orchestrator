73d7e623c21e4d020442519834fcc4e6f3fe27f8
Ikenna Omekam <iomekam@github.com>
2021-02-25 14:14:55 -0500

    consumer test

---
+++ spec/consumers/shopify_product_deleted_consumer_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+RSpec.describe ShopifyProductDeletedConsumer do
+  let(:message) do
+    {
+      sender_id: Faker::Alphanumeric.alpha,
+    }
+  end
+
+  subject(:consumer) { described_class.new }
+
+  before(:each) do
+    ActiveJob::Base.queue_adapter = :test
+  end
+
+  it "should have correct queue name" do
+    expect(described_class.get_queue_name).to eq("consumer_ecommerce_orchestrator_shopify_product_deleted")
+  end
+
+  it "should enqueue shopify product created job" do
+    consumer.process(message)
+    expect(ShopifyProductDeletedJob).to have_been_enqueued.with({
+                                                        product_id: message[:sender_id],
+                                                      })
+  end
+end
