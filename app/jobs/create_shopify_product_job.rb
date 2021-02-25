7857c6e3f4caf2291f19f344f91ab312cd280753
Ikenna Omekam <iomekam@github.com>
2021-02-25 01:56:13 -0500

    save

--- app/jobs/create_shopify_product_job.rb
+++ app/jobs/create_shopify_product_job.rb
@@ -1,7 +1,7 @@
 class CreateShopifyProductJob < ApplicationJob
   queue_as :ecommerce_orchestrator_create_shopify_product
 
-  def perform(user_id:, name:, price:, type:, tags:)
+  def perform(user_id:, name:, price:, type:)
     Hutch.connect
     Hutch.publish("shopify.product.create",
       vendor_user_id: user_id,
